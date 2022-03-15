079bc0d67c8d239a20c948aac585ac809dbbd484
Emir Cengiz <emircengiz21@gmail.com>
2022-03-15 15:22:03 +0300

    Add: Better button handling. TODO: Add new UI elements

--- Emix.Tests/MyGame.cs
+++ Emix.Tests/MyGame.cs
@@ -1,20 +1,45 @@
-﻿using Emix.Windowing;
+﻿using Emix.Graphics.GUI;
+using Emix.Windowing;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
 
 namespace Emix.Tests
 {
     public class MyGame : EmixScene
     {
+        private BasicButton newButton;
+        private BasicButton loadButton;
+        private BasicButton settingsButton;
+        private BasicButton exitButton;
+        
+        
         public MyGame()
         {
-            Window = new GameWindow("Bruvkekw");
+            Window = new GameWindow("Emix: Visual Novel Engine");
         }
         protected override void Initialize()
         {
+            newButton = new BasicButton("Play");
+            exitButton = new BasicButton("Exit");
+            settingsButton = new BasicButton("Settings");
+            loadButton = new BasicButton("Load");
+            
+            newButton.Position = new Vector2f(20, 300);
+            loadButton.Position = new Vector2f(20, 350);
+            settingsButton.Position = new Vector2f(20, 400);
+            exitButton.Position = new Vector2f(20, 450);
+            
             base.Initialize();
         }
 
         protected override void Update(GameTime gameTime)
         {
+            newButton.Draw(Window);
+            exitButton.Draw(Window);
+            settingsButton.Draw(Window);
+            loadButton.Draw(Window);
+            
             base.Update(gameTime);
         }
     }
--- Emix/Graphics/GUI/BasicButton.cs
+++ Emix/Graphics/GUI/BasicButton.cs
@@ -1,4 +1,5 @@
 using System;
+using Emix.Windowing;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -15,15 +16,45 @@ namespace Emix.Graphics.GUI
         private readonly Color _textColor;
         private readonly Color _hoverColor;
         private readonly Color _pressedColor;
+        public Font font { get; set; }
         private bool _isPressed;
 
-        public BasicButton(string text, Font font, Color backgroundColor, Color textColor, Color hoverColor, Color pressedColor) 
+        
+        
+        public BasicButton(string text) 
+            : base(new Texture("Resources/button.png"))
+        {
+            font = new Font(("Resources/arial.ttf"));
+            _text = new Text(text, font);
+            _text.CharacterSize = 20;
+            _text.Color = Color.White;
+            _text.Position = new Vector2f(10, 10);
+            
+
+            _background = new RectangleShape();
+            _background.FillColor = Color.Black;
+            _background.Position = Position;
+            _background.Size = new Vector2f(_text.GetGlobalBounds().Width + 20, _text.GetGlobalBounds().Height + 20);
+            _background.OutlineColor = Color.White;
+            _background.OutlineThickness = 2;
+            _backgroundColor = Color.Black;
+            _textColor = Color.White;
+            _hoverColor = new Color(192, 192, 192);
+            _pressedColor = new Color(128, 128, 128);;
+        }
+        
+        public BasicButton(string text, 
+            Color backgroundColor,  
+            Color textColor, 
+            Color hoverColor, 
+            Color pressedColor) 
             : base(new Texture("Resources/button.png"))
         {
             _text = new Text(text, font);
             _text.CharacterSize = 20;
             _text.Color = textColor;
             _text.Position = new Vector2f(10, 10);
+            font = new Font(("Resources/arial.ttf"));
 
             _background = new RectangleShape();
             _background.FillColor = backgroundColor;
@@ -35,6 +66,32 @@ namespace Emix.Graphics.GUI
             _hoverColor = hoverColor;
             _pressedColor = pressedColor;
         }
+        
+        
+        public BasicButton(string text, Font font,
+            Color backgroundColor,  
+            Color textColor, 
+            Color hoverColor, 
+            Color pressedColor) 
+            : base(new Texture("Resources/button.png"))
+        {
+            _text = new Text(text, font);
+            _text.CharacterSize = 20;
+            _text.Color = textColor;
+            _text.Position = new Vector2f(10, 10);
+            //font = new Font(("Resources/arial.ttf"));
+
+            _background = new RectangleShape();
+            _background.FillColor = backgroundColor;
+            _background.Position = Position;
+            _background.Size = new Vector2f(_text.GetGlobalBounds().Width + 20, _text.GetGlobalBounds().Height + 20);
+            
+            _backgroundColor = backgroundColor;
+            _textColor = textColor;
+            _hoverColor = hoverColor;
+            _pressedColor = pressedColor;
+        }
+
 
         public void Update(Vector2f mousePosition)
         {
@@ -63,13 +120,70 @@ namespace Emix.Graphics.GUI
             
             Console.WriteLine(mousePosition.X + " " +mousePosition.Y);
         }
+        
+        
+        public new virtual void Draw(GameWindow window)
+        {
+            //states.Transform *= Transform;
+            var mousePosition = Mouse.GetPosition(window);
+            window.Draw(_background, RenderStates.Default);
+            window.Draw(_text, RenderStates.Default);
+            _background.Position = Position;
+            _text.Position = new Vector2f(Position.X + 10, Position.Y + 10);
+            
+            
+            if (_background.GetGlobalBounds().Contains(mousePosition.X, mousePosition.Y))
+            {
+                if (Mouse.IsButtonPressed(Mouse.Button.Left))
+                {
+                    _isPressed = true;
+                    _background.FillColor = _pressedColor;
+                }
+                else
+                {
+                    _isPressed = false;
+                    _background.FillColor = _hoverColor;
+                }
+            }
+            else
+            {
+                _isPressed = false;
+                _background.FillColor = _backgroundColor;
+            }
+            
+            Console.WriteLine(mousePosition.X + " " +mousePosition.Y);
+        }
 
-        public new virtual void Draw(RenderTarget target, RenderStates states)
+        public new virtual void Draw(RenderTarget target, RenderStates states, Vector2f mousePosition)
         {
             //states.Transform *= Transform;
             
             target.Draw(_background, states);
             target.Draw(_text, states);
+            _background.Position = Position;
+            _text.Position = new Vector2f(Position.X + 10, Position.Y + 10);
+            
+            
+            if (_background.GetGlobalBounds().Contains(mousePosition.X, mousePosition.Y))
+            {
+                if (Mouse.IsButtonPressed(Mouse.Button.Left))
+                {
+                    _isPressed = true;
+                    _background.FillColor = _pressedColor;
+                }
+                else
+                {
+                    _isPressed = false;
+                    _background.FillColor = _hoverColor;
+                }
+            }
+            else
+            {
+                _isPressed = false;
+                _background.FillColor = _backgroundColor;
+            }
+            
+            Console.WriteLine(mousePosition.X + " " +mousePosition.Y);
         }
 
         public void OnClick()
